9e7398ecd57a25a39bc22a719004a859528b2678
Akis Kontakis <akis@inked.gr>
2021-01-12 09:46:37 +0200

    Add project files.

---
+++ AuthServer.NET/Middleware/ErrorHandlerMiddleware.cs
@@ -0,0 +1,49 @@
+﻿using AuthServer.NET.Models.Exceptions;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace AuthServer.NET.Middleware
+{
+    public class ErrorHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ErrorHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                var response = context.Response;
+                response.ContentType = "application/json";
+                switch (ex)
+                {
+                    case AppException c:
+                        break;
+                    case KeyNotFoundException c:
+                        response.StatusCode = (int)HttpStatusCode.NotFound;
+
+                        break;
+
+                    default:
+                        response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        break;
+                }
+                var result = JsonSerializer.Serialize(new { message = ex?.Message});
+                await response.WriteAsync(result);
+            }
+        }
+    }
+}
